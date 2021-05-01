884e4cc25b34afb6fd1d0cca3a57906b9236363f
Brandon Dutton <brandonkdutton@gmail.com>
2021-04-30 17:23:50 -0700

    front end works

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,14 +1,17 @@
-import React, { FC, useState, useRef, RefObject } from 'react';
+import { read } from "node:fs";
+import React, { FC, useState, useRef, RefObject, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const App: FC = () => {
   const [fileInputKey, setFileInputKey] = useState<string>(uuidv4());
+  const [imgUrl, setImgUrl] = useState<string>();
+  const [localImg, setLocalImg] = useState<string>();
   const [img, setImg] = useState<string>();
   const fileInput: RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
 
   // validate and preview image file when user selects one
-  const onFileChange = (): void => {
-    const files = fileInput?.current?.files;
+  const onLocalFileChange = (): void => {
+    const files: FileList = fileInput?.current?.files as FileList;
     if (!files)
       return;
 
@@ -23,11 +26,30 @@ const App: FC = () => {
       // trick react into clearing the file input by re-rendering it.
       // based on: https://github.com/redux-form/redux-form/issues/769
       setFileInputKey(uuidv4());
-      setImg(undefined);
       return;
     }
+    setLocalImg(fileObj);
+  };
 
-    setImg(fileObj);
+  const onImgUrlSelected = (): void => {
+    setFileInputKey(uuidv4());
+    setImg(imgUrl);
+  };
+
+  useEffect(() => {
+    setImg(localImg ?? imgUrl);
+  }, [localImg]);
+
+  const submitTransformation = async (): Promise<void> => {
+    const uri = `${process.env.REACT_APP_API_URI}/services/imageTransformer`;
+    const body = new FormData();
+    body.append('ImgUpload', localImg as string);
+    body.append('ImgUrl', imgUrl as string);
+
+    const req = await fetch(uri, {
+      method: "POST",
+      body: body
+    });
   };
 
   return (
@@ -43,15 +65,26 @@ const App: FC = () => {
             id="fileUpload"
             ref={fileInput}
             key={fileInputKey}
-            onChange={onFileChange}
+            onChange={onLocalFileChange}
           />
         </fieldset>
         <h3>Or...</h3>
         <fieldset>
           <label htmlFor="imageUrlInput">enter image URL</label>
           <br />
-          <input type="text" name="imageUrl" id="imageUrlInput" />
-          <input type="button" value="Set URL" />
+          <input type="text" name="imageUrl" id="imageUrlInput" value={imgUrl} onChange={({ target }) => setImgUrl(target.value)} />
+          <input type="button" value="Set URL" onClick={onImgUrlSelected} />
+        </fieldset>
+        <h3>Select a transformation to apply</h3>
+        <fieldset>
+          <select name="transformation" id="transformationSelect">
+            <option selected value="None">None</option>
+            <option value="Saturate">Saturate</option>
+            <option value="Monochrome">Monochrome</option>
+            <option value="Brighten">Brighten</option>
+            <option value="Darken">Darken</option>
+          </select>
+          <input type="button" value="Submit transformation" onClick={submitTransformation} />
         </fieldset>
       </form>
 
